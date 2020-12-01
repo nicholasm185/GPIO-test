197e205413c6d4fa00b9bab88d56d6b197db75eb
nicholasm185 <nicholasm185@gmail.com>
2020-12-01 18:41:00 +0700

    reading magnetometer works

---
+++ magnet.py
@@ -0,0 +1,5 @@
+import time
+import py_qmc5883l
+sensor = py_qmc5883l.QMC5883L(output_data_rate=py_qmc5883l.ODR_100HZ, output_range=py_qmc5883l.RNG_8G)
+while True:
+    print(sensor.get_magnet_raw())
